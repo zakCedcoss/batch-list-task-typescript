f8749230d158fe79f0850ed587afa0af441c51df
Zak <zeeshan@cedcommerce.com>
2022-09-21 16:34:04 +0530

    error handled

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import Group from "./components/Group";
-import { GroupType } from "./types";
+import { GroupType, RowType } from "./types";
 
 function App() {
   const [groupsArray, setGroupsArray] = useState<GroupType[]>([
@@ -11,28 +11,41 @@ function App() {
     },
   ]);
   const [message, setMessage] = useState<string>("");
+  const [errorMessage, setErrorMessage] = useState<string>("");
+
+  useEffect(() => {
+    let interval = setTimeout(() => {
+      setErrorMessage("");
+    }, 3000);
+
+    return () => clearInterval(interval);
+  }, [errorMessage]);
 
   useEffect(() => {
     let s: string = "";
-    groupsArray.map((g, j) => {
-      g.rowsArray?.map((r, i) => {
-        const { firstSelect, secondSelect, thirdSelect } = r;
-        s += `${firstSelect} ${secondSelect} ${thirdSelect}`;
+    groupsArray?.map((g: GroupType, j: number) => {
+      if (j > 0) {
         if (
-          g.rowsArray?.length > 1 &&
-          g.rowsArray.length - 1 !== i &&
-          thirdSelect !== ""
+          g.rowsArray.length &&
+          g.rowsArray[0].firstSelect &&
+          g.rowsArray[0].secondSelect &&
+          g.rowsArray[0].thirdSelect
         ) {
-          s += " AND ";
+          s += " OR ";
+        }
+      }
+
+      g.rowsArray?.map((r: RowType, i: number) => {
+        const { firstSelect, secondSelect, thirdSelect } = r;
+        if (i > 0) {
+          if (firstSelect !== "" && secondSelect !== "" && thirdSelect !== "") {
+            s += " AND ";
+          }
+        }
+        if (firstSelect && secondSelect && thirdSelect) {
+          s += `${firstSelect} ${secondSelect} ${thirdSelect}`;
         }
       });
-      if (
-        groupsArray.length > 1 &&
-        groupsArray.length - 1 !== j &&
-        groupsArray[j].rowsArray[0].firstSelect !== ""
-      ) {
-        s += " OR ";
-      }
     });
 
     setMessage(s);
@@ -50,12 +63,35 @@ function App() {
     setGroupsArray([...groupsArray, newGroup]);
   };
 
-  console.log(groupsArray);
+  const handleSubmit = (): void => {
+    let error: Boolean = false;
+    groupsArray.map((g) => {
+      g.rowsArray.map((r) => {
+        if (
+          r.firstSelect === "" ||
+          r.secondSelect === "" ||
+          r.thirdSelect === ""
+        ) {
+          error = true;
+        }
+      });
+    });
+    if (error) {
+      console.log("Please fill all fields !!!");
+      setErrorMessage("Please fill all fields !!!");
+    } else {
+      console.log(groupsArray);
+      setErrorMessage("Check CONSOLE.LOG in Developer Tool");
+    }
+  };
+
+  // console.log(groupsArray);
   // console.log(message);
 
   return (
     <div className="App">
-      <div className="group-head">{message}</div>
+      <div className="error">{errorMessage}</div>
+      {message && <div className="group-head">{message}</div>}
       <button onClick={handleAddGroup}>Add Group</button>
       {groupsArray.map((group) => {
         return (
@@ -67,6 +103,7 @@ function App() {
           />
         );
       })}
+      <button onClick={handleSubmit}>Submit</button>
     </div>
   );
 }
